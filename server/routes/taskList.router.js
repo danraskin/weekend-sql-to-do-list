11de93dad308e8872ec0fe269eef9043816c37d8
Dan Raskin <Danieltraskin@gmail.com>
2022-09-09 09:20:56 -0500

    setup complete

---
+++ server/routes/taskList.router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const taskListRouter = express.Router();
+
+// DB CONNECTION
+const pool = require('../modules/pool');
+
+//GET
+
+//POST
+
+//PUT
+
+//DELETE
+
+// export
+module.exports = taskListRouter;
