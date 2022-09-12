f374fb79808caf7a2aa9f9d623baa19ec2f252e2
Dan Raskin <Danieltraskin@gmail.com>
2022-09-11 23:01:27 -0500

    dependent task inputs working! all tasks have relative id counters, connecting them in a tree of parent-child-sibling relationships. Formatting is sitll bad, so ids are displayed to compensate

--- server/routes/taskList.router.js
+++ server/routes/taskList.router.js
@@ -10,7 +10,7 @@ taskListRouter.get ('/', (req, res) => {
     console.log('in GET /tasks');
 
     //change order to "idrel" if i add detailed sub-task menu
-    let queryText = 'SELECT * FROM "taskList" ORDER BY "taskStatus", "id";'
+    let queryText = 'SELECT * FROM "taskList" ORDER BY "taskStatus", "idrel";'
     pool.query(queryText).then(result => {
         res.send(result.rows);
     })
@@ -18,7 +18,18 @@ taskListRouter.get ('/', (req, res) => {
         console.log('error GET /tasks', error);
         res.sendStatus(500);
     });
+})
 
+taskListRouter.get ('/counterset', (req, res) => {
+    let queryText = 'SELECT MAX("idrel") FROM "taskList";'
+    pool.query(queryText).then(result => {
+        console.log(result);
+        res.send(result.rows);
+    })
+    .catch(error => {
+        console.log('error GET /tasks/counterset', error);
+        res.sendStatus(500);
+    })
 })
 
 //POST
@@ -26,15 +37,13 @@ taskListRouter.get ('/', (req, res) => {
 taskListRouter.post ('/', (req, res) => {
     let newTask = req.body;
 
-    //set status ternary logic from koalaHolla project
-    //let transfer = (req.body.ready_to_transfer === 'Y') ? true : false;
 
     console.log('in POST /tasks: ', newTask);
 
-    let queryText = `INSERT INTO "taskList" ("task", "taskLength", "notes")
-                   VALUES ($1, $2, $3);`;
+    let queryText = `INSERT INTO "taskList" ("idrel", "task", "taskLength", "notes")
+                   VALUES ($1, $2, $3, $4);`;
 
-    pool.query(queryText, [newTask.task, newTask.taskLength, newTask.notes])
+    pool.query(queryText, [newTask.idrel, newTask.task, newTask.taskLength, newTask.notes])
         .then(result => {
             res.sendStatus(201);
         })
@@ -65,6 +74,30 @@ taskListRouter.put ('/:id', (req, res) => {
             res.sendStatus(500);
         });
 })
+
+taskListRouter.put ('/idrelchild/:id', (req, res) => {
+    console.log(`Changing idrel: id ${req.params.id}`);
+    console.log(req.body);
+    let updateId = req.params.id;
+    // let updateIdRel = req.body.idrel;
+    let updateIdRelChild = req.body.idrelchild;
+
+    //updates idrelchild
+    const queryText = `
+        UPDATE "taskList"
+            SET "idrelchild" = $1
+            WHERE "id" = $2;
+    `
+    pool.query(queryText, [updateIdRelChild, updateId])
+        .then(result => {
+            res.sendStatus(200);
+        })
+        .catch(error => {
+            console.log('Error PUT /tasks/IDrelchild', error);
+            res.sendStatus(500);
+        });
+})
+
 
 //DELETE
 
