8c0f37a87317dd1352f4e57d57d79bc26c6f99b4
Dan Raskin <Danieltraskin@gmail.com>
2022-09-16 12:30:32 -0500

    pool configured for Heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,12 +1,23 @@
 const pg = require('pg');
+const url = require('url');
 
-const config = {
-  database: 'weekend-to-do-app', 
-  host: 'localhost', 
-  port: 5432, 
-  max: 10, 
-  idleTimeoutMillis: 30000 
-};
+//HEROKU CONFIG
+let config {}
+
+if (process.env.DATABASE_URL) {
+  config = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  };
+} else {
+  config = {
+    database: 'weekend-to-do-app', 
+    host: 'localhost', 
+    port: 5432, 
+    max: 10, 
+    idleTimeoutMillis: 30000 
+  }
+}
 
 const pool = new pg.Pool(config);
 
