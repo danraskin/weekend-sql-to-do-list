c486d17d3350742ec5284297c99bcb301a1a87c0
Dan Raskin <Danieltraskin@gmail.com>
2022-09-21 11:07:50 -0500

    updated pool

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,7 +1,5 @@
 const pg = require('pg');
 
-const Pool = pg.Pool;
-
 const url = require('url');
 
 //HEROKU CONFIG
@@ -17,9 +15,9 @@ if (process.env.DATABASE_URL) {
     database: 'weekend-to-do-app', 
     host: 'localhost', 
     port: 5432, 
-    max: 10, 
-    idleTimeoutMillis: 30000 
-  }
+    // max: 10, 
+    // idleTimeoutMillis: 30000 
+  };
 }
 
 const pool = new pg.Pool(config);
@@ -30,6 +28,7 @@ pool.on("connect", () => {
 
 pool.on("error", (err) => {
   console.log("error connecting to postgres", err);
+  process.exit(-1);
 });
 
 module.exports = pool;
